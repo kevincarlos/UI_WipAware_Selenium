397f467e44de314b916c1ed93dd658f56b2a94e9
Kurt Carlos <kurt.carlos@aderant.com>
2023-06-13 09:22:12 +1200

    Implemented SearchResult POM

---
+++ SearchControl/SearchResult.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_WipAware.Results
+
+
+{
+    class SearchResult
+    {
+        public readonly By primaryText = By.CssSelector("#search-control-results > li > div > div > div.search-result-text");
+        public readonly By secondayText = By.CssSelector("#search-control-results > li > div > div > div.secondary-text");
+        public readonly By tertiaryText = By.CssSelector("#search-control-results > li > div > div > div.tertiary-text");
+
+        private readonly IWebDriver driver;
+        private IList<IWebElement> list;
+
+
+        public SearchResult(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void FindAndClick(By by,String text)
+        {
+            list = driver.FindElements(by);
+            foreach (IWebElement e in list)
+            {
+                if (e.Text == text)
+                {
+                    e.Click();
+                }
+            }
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using UI_WipAware.Elements;
+using UI_WipAware.Results;
 
 namespace UI_WipAware
 {
@@ -51,123 +52,28 @@ namespace UI_WipAware
             // possibly add rights to "Wip Aware User" policy.
         }
 
-
-
-        [TestMethod]
-        [Description("This will trigger an Advanced Search for Arden Industries in the client.matter field " +
-                    "and select General(1) as the matter and compare the results of WipTotal, WipLessPrebill " +
-                    "and ARtotal in the Bill Summary")]
-        public void AdvanceSearch()
-        {
-            //check if advance search is clickable (Explicit Wait) then click Advanced Search Button
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector(HeaderButtons.CSSAdvancedSearchButton), Timeout);
-            WipAwareDriver.FindElement(By.CssSelector(HeaderButtons.CSSAdvancedSearchButton)).Click();
-
-            //wait if the client.matter element is available then click
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector(AdvancedFilter.CSSMatterIdSearchButton), Timeout);
-            WipAwareDriver.FindElement(By.CssSelector(AdvancedFilter.CSSMatterIdSearchButton)).Click();
-
-            //wait for the client.matter popup element is available then type Arden
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector(AdvancedFilter.CSSMatterIdPopUpInput), Timeout);
-            WipAwareDriver.FindElement(By.CssSelector(AdvancedFilter.CSSMatterIdPopUpInput)).SendKeys("Arden");
-
-            //wait for the populated client.matter that was typed in
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector("#search-control-results > li:nth-child(2) > div > div"), Timeout);
-
-            //gets the count number of list under the dropdown
-            var count = WipAwareDriver.FindElements(By.CssSelector("#search-control-results > li")).Count;
-
-            //implement a search for "General (1)" matter then click if it is "General (1)"
-            for (int i = 1; i <= count; i++)
-            {
-                var isArdenGeneral = WipAwareDriver.FindElement(By.CssSelector("#search-control-results > li:nth-child(" + i + ") > div > div > div.secondary-text")).Text;
-                //Console.WriteLine(isArdenGeneral);
-                if (isArdenGeneral == "General (1)")
-                {
-                    WipAwareDriver.FindElement(By.CssSelector("#search-control-results > li:nth-child(" + i + ") > div > div")).Click();
-                }
-            }
-            //wait and assert if the matter selected in Arden General (1)
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector("#a-advanced-filter > div.a-advanced-filter-content > div > div:nth-child(1) > div > div.a-multiselect-container > div > div.a-multiselect-item-text > div.a-multiselect-item-secondarytext"), Timeout);
-            string isGeneral = WipAwareDriver.FindElement(By.CssSelector("#a-advanced-filter > div.a-advanced-filter-content > div > div:nth-child(1) > div > div.a-multiselect-container > div > div.a-multiselect-item-text > div.a-multiselect-item-secondarytext"))
-                .Text;
-            Assert.AreEqual(isGeneral, "General (1)", "Fail");
-
-            //wait and click search button
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.XPath(AdvancedFilter.XpathSearchButton), Timeout);
-            WipAwareDriver.FindElement(By.XPath(AdvancedFilter.XpathSearchButton)).Click();
-
-            //wait for the results for Arden General (1) in the grid then get the values on the grid
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector
-                ("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(3) > div > div > div.a-row-item-content.a-linear-transition > div > div.a-primary-text > span:nth-child(2)"), Timeout);
-
-            var WipTotal = WipAwareDriver.FindElement(By.CssSelector("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(4) > span")).Text;
-            var WipLessPrebill = WipAwareDriver.FindElement(By.CssSelector("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(5) > a")).Text;
-            var ARtotal = WipAwareDriver.FindElement(By.CssSelector("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(8) > span")).Text;
-
-            //Click the grid result of search for Arden
-            WipAwareDriver.FindElement(By.CssSelector
-                ("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(3) > div > div > div.a-row-item-content.a-linear-transition > div > div.a-primary-text > span:nth-child(2)")).Click();
-
-            //wait for the popup window on the side then get the values to compare
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector("#smartForm-content > div > div:nth-child(4) > div > span.a-graph-title > span.a-summary-total-value"), Timeout);
-
-            var SideWindowWipTotal = "test";
-            //var SideWindowWipTotal = WipAwareDriver.FindElement(By.CssSelector("#smartForm-content > div > div:nth-child(4) > div > span.a-graph-title > span.a-summary-total-value")).Text;
-            var SideWindowWipLessPrebill = WipAwareDriver.FindElement(By.CssSelector("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(5) > a")).Text;
-            var SideWindowARtotal = WipAwareDriver.FindElement(By.CssSelector("#actionGrid > div.k-grid-content.k-auto-scrollable > table > tbody > tr > td:nth-child(8) > span")).Text;
-
-            //Screenshot
-            //WebDriverExtensions.TakeScreenshot(WipAwareDriver);
-
-            //compare values using assertion
-            //WebDriverExtensions.AssertFailScreenshot(WipAwareDriver, SideWindowWipTotal, WipTotal);
-            //WebDriverExtensions.AssertFailScreenshot(WipAwareDriver,SideWindowWipLessPrebill, WipLessPrebill);
-            //WebDriverExtensions.AssertFailScreenshot(WipAwareDriver,SideWindowARtotal, ARtotal);
-
-            //Assert.AreEqual(SideWindowWipTotal, WipTotal);
-            //Assert.AreEqual(SideWindowWipLessPrebill, WipLessPrebill);
-            //Assert.AreEqual(SideWindowARtotal, ARtotal);
-
-            //sleep then close driver
-            //Thread.Sleep(3000);
-            Stop();
-        }
-
         [TestMethod]
         [Description("Testing POM")]
 
-        public void Test2()
+        public void AdvancedSearch()
         {
             HeaderButtons headerButtons = new HeaderButtons(WipAwareDriver);
             AdvancedFilter advancedFilter = new AdvancedFilter(WipAwareDriver);
             AdvancedSearchResults advancedSearchResults = new AdvancedSearchResults(WipAwareDriver);
             MatterSummary matterSummary = new MatterSummary(WipAwareDriver);
+            SearchResult searchResult = new SearchResult(WipAwareDriver);
             
 
             headerButtons.clickAdvancedSearchButton();
             advancedFilter.clickMatterIdSearchButton();
             advancedFilter.sendKeysMatterIdPopUpInput("Arden"); //DDT / hardcode later
-
-            var count = advancedFilter.getClientMatterList();
-
-            //implement a search for "General (1)" matter then click if it is "General (1)"
-            WebDriverExtensions.WaitForElement(WipAwareDriver, By.CssSelector("#search-control-results > li:nth-child(2) > div > div"), Timeout);
-            for (int i = 1; i <= count; i++)
-            {
-                var isArdenGeneral = WipAwareDriver.FindElement(By.CssSelector("#search-control-results > li:nth-child(" + i + ") > div > div > div.secondary-text")).Text;
-                //Console.WriteLine(isArdenGeneral);
-                if (isArdenGeneral == "General (1)")
-                {
-                    WipAwareDriver.FindElement(By.CssSelector("#search-control-results > li:nth-child(" + i + ") > div > div")).Click();
-                }
-            }
-
+            searchResult.FindAndClick(searchResult.secondayText,"General (1)");
             advancedFilter.Waitfor(advancedFilter.ClientMatterFilterContent);
             advancedFilter.clickSearchButton();
             advancedSearchResults.clickClientMatterFirstResult();
-
-
+            //implement Assert
+            Stop();
+            
         }
         
       
